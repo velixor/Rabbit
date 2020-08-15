7c892582d6149edb1b06a384ef892ada89741c38
velixor <velixor.work@gmail.com>
2020-08-15 16:56:36 +0300

    add Consumer

---
+++ Consumer/Receiver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    internal static class Receiver
+    {
+        private const string Queue = "BasicQueue";
+
+        private static void Main()
+        {
+            var connectionFactory = new ConnectionFactory {HostName = "localhost"};
+
+            using var connection = connectionFactory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += ConsumerOnReceived;
+
+            channel.BasicConsume(Queue, true, consumer);
+
+            Console.WriteLine("End of receiving");
+        }
+
+        private static void ConsumerOnReceived(object? sender, BasicDeliverEventArgs e)
+        {
+            Console.WriteLine($"Message received: {GetString(e.Body)}");
+        }
+
+        private static string GetString(in ReadOnlyMemory<byte> body)
+        {
+            return Encoding.UTF8.GetString(body.ToArray());
+        }
+    }
+}
