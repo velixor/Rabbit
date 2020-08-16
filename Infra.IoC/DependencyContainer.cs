3c6991a25aef501cf62ac797e6ebb0c960bdc2c1
velixor <velixor.work@gmail.com>
2020-08-16 17:01:30 +0300

    add Banking microservice

---
+++ Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Core.Bus;
+using Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMqBus>();
+        }
+    }
+}
