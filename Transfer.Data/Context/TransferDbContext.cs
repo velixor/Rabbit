6e480155c792c2f3f416b0aacf4b3d3576143eeb
velixor <velixor.work@gmail.com>
2020-08-17 21:46:28 +0300

    setup TransferDbContext

--- Transfer.Data/Context/TransferDbContext.cs
+++ Transfer.Data/Context/TransferDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Transfer.Domain.Models;
 
 namespace Transfer.Data.Context
 {
     public class TransferDbContext : DbContext
     {
+        public DbSet<TransferLog> TransferLogs { get; set; }
         public TransferDbContext(DbContextOptions options) : base(options)
         {
         }
